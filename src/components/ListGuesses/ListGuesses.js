08132a270fc499e736399a104c3c2385c598b04b
Asad Ali <asadali.asif.1@gmail.com>
2024-05-03 15:31:36 +0100

    Make key unique

--- src/components/ListGuesses/ListGuesses.js
+++ src/components/ListGuesses/ListGuesses.js
@@ -3,8 +3,8 @@ import React from "react";
 function ListGuesses({ guessList }) {
   return (
     <div className="guess-results">
-      {guessList.map((guess) => (
-        <p className="guess" key={guess}>
+      {guessList.map((guess, index) => (
+        <p className="guess" key={`${index}-guess`}>
           {guess}
         </p>
       ))}
